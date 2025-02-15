510fbe6f15bdf4dbe87e10cb584415e4e6adc552
bryangurr <150076870+bryangurr@users.noreply.github.com>
2025-02-15 00:09:42 -0700

    Fix select statement and add required fields and MaxLength.

--- Migrations/20250215065825_Initial.cs
+++ Migrations/20250215065825_Initial.cs
@@ -19,9 +19,9 @@ namespace Mission06_Gurr.Migrations
                     Year = table.Column<int>(type: "INTEGER", nullable: false),
                     Director = table.Column<string>(type: "TEXT", nullable: false),
                     Rating = table.Column<string>(type: "TEXT", nullable: false),
-                    Edited = table.Column<bool>(type: "INTEGER", nullable: false),
-                    LentTo = table.Column<string>(type: "TEXT", nullable: false),
-                    Notes = table.Column<string>(type: "TEXT", nullable: false)
+                    Edited = table.Column<bool>(type: "INTEGER", nullable: true),
+                    LentTo = table.Column<string>(type: "TEXT", nullable: true),
+                    Notes = table.Column<string>(type: "TEXT", nullable: true)
                 },
                 constraints: table =>
                 {
--- Models/Movie.cs
+++ Models/Movie.cs
@@ -4,14 +4,24 @@ namespace Mission06_Gurr.Models
 {
     public class Movie
     {
+        // Getters and Setters to build instances of class
         [Key]
+        [Required]
         public string Title { get; set; }
+        [Required]
         public string Category { get; set; }
+        [Required]
         public int Year { get; set; }
+        [Required]
         public string Director {  get; set; }
+        [Required]
+
         public string Rating { get; set; }
-        public bool Edited { get; set; }
-        public string LentTo {  get; set; }
-        public string Notes { get; set; }
+        [Required]
+        // Nullable entries
+        public bool? Edited { get; set; }
+        public string? LentTo {  get; set; }
+        [MaxLength(25)]
+        public string? Notes { get; set; }
     }
 }
